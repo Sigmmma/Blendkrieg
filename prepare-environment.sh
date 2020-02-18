c8c0d84cd37dd2ae489b5024a2e9ded896c57879
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-18 14:35:52 +0100

    Add depdencies.txt and a script to prepare the environment for loading in Blender

---
+++ prepare-environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Installs the required libraries to the ./lib folder.
+# This makes it so no additional steps are required to run the project in
+# blender. And it avoids the need to install reclaimer and its deps into
+# site-packages.
+python3 -m pip install -r dependencies.txt --target="./lib"
