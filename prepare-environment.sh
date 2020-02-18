6949f97da4035fef1ec2e3602310e3fc96bb0c47
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-18 14:52:59 +0100

    No deps, because the deps are big and we don't need them for now.

--- prepare-environment.sh
+++ prepare-environment.sh
@@ -4,4 +4,4 @@
 # This makes it so no additional steps are required to run the project in
 # blender. And it avoids the need to install reclaimer and its deps into
 # site-packages.
-python3 -m pip install -r dependencies.txt --target="./lib"
+python3 -m pip install -r dependencies.txt --target="./lib" --no-deps
