94937d0f7813b9aaac31e063240c74d7b795083e
Mia Moretti <mimickal.dev@gmail.com>
2020-03-08 19:29:33 -0400

    Support specifying test Blender location in multiple ways

--- test/test.py
+++ test/test.py
@@ -1,24 +1,39 @@
-#!env python3
-'''
-Runs all (or a subset of) the tests inside Blender.
-
-Blendkrieg depends on Blender's internal bpy module, and thus the tests do too.
-This script makes it a little easier to do that. We assume this script and
-run-pocha.py are co-located in the test directory.
-'''
-
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+import os
 from pathlib import Path
 from shutil import which
 import subprocess
 import sys
 
-if __name__ == '__main__':
-	run_pocha = Path(__file__).resolve().parent.joinpath('run-pocha.py')
+DESC = '''
+	Runs all (or a subset of) the tests inside Blender.
 
-	# We could be more robust here and support providing Blender's path as an
-	# argument, but FU.
-	blender = which('blender')
+	Blendkrieg depends on Blender's internal bpy module, and thus the tests do
+	too. This script makes it a little easier to run our tests inside Blender.
+	We assume this script and run-pocha.py are co-located in the test directory.
+	'''
+
+if __name__ == '__main__':
+	parser = ArgumentParser(description=DESC)
+	parser.add_argument('-b', '--blender', type=str, default=None, help='''
+		Path to Blender executable. This can also be specified by setting the
+		environment variable BLENDER_PATH. If not specified, this script will
+		search for a blender executable on the system.
+	''')
+	args = parser.parse_args()
+
+	# Support reading Blender location in multiple ways
+	if args.blender:
+		blender = args.blender
+	elif os.environ.get('BLENDER_PATH'):
+		blender = os.environ.get('BLENDER_PATH')
+	else:
+		blender = which('blender')
+
 	if blender is None:
-		raise Exception('Cannot find blender')
+		raise Exception('Cannot find Blender executable')
+
+	run_pocha = Path(__file__).resolve().parent.joinpath('run-pocha.py')
 
 	subprocess.run([blender, '--background', '--python', str(run_pocha)])
